16bee8242fda6b9f49082ccc6c5499299c8e2ac7
Aida <terenteva.aida.2401@gmail.com>
2026-04-01 21:21:56 +0300

    1

---
+++ task 1.py
@@ -0,0 +1,29 @@
+# todo: Определить в коде переменные:
+
+# 1. Целочисленного типа
+
+int_var = 19
+
+# 2. Вещественного типа
+
+float_var = 3.1289
+
+# 3. Логического типа
+
+bool_var = True
+
+# 4. Строкового типа
+
+str_var = "Hello World!"
+
+# 5. Пустого типа
+
+none_var = None
+
+# Вывести их типы.
+
+print(f"int_var имеет тип: {type(int_var)}")
+print(f"float_var имеет тип: {type(float_var)}")
+print(f"bool_var имеет тип: {type(bool_var)}")
+print(f"str_var имеет тип: {type(str_var)}")
+print(f"none_var имеет тип: {type(none_var)}")
