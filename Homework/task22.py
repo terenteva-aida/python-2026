f31e0e16f5a88ffad43f15620301ed8ff29dd56c
Aida <terenteva.aida.2401@gmail.com>
2026-04-16 23:27:52 +0300

    task 22

--- Homework/task22.py
+++ Homework/task22.py
@@ -2,5 +2,17 @@
 #  приветствие "Hello", которое до этого записано в файл text.txt
 #  через метод write()
 
-f = open("text.txt", "w+t")
+
+# t - работа со строками, а не байтами
+f = open(r'C:/Users/User/OneDrive/Рабочий стол/Python/repo/Homework/text.txt', "w+t")
 f.write("Hello\n")
+
+# Перемещение указателя в начало файла
+f.seek(0)
+
+# Читаем содержимое файла
+content = f.read
+
+# end='' нужно, чтобы не было пустой строки, так как есть \n выше
+print(content, end='')
+f.close()
