54ef0e9d45b83c4b9f8153aac215715e701f80cc
Aida <terenteva.aida.2401@gmail.com>
2026-05-22 20:53:20 +0300

    новый скрипт

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 # -- Таблица: аккаунт
-# CREATE TABLE account (
+# CREATE TABLE user_account (
 #     id_account SERIAL PRIMARY KEY,
 #     login VARCHAR(255) NOT NULL UNIQUE,
 #     pswd VARCHAR(255) NOT NULL,
@@ -9,27 +9,19 @@
 #     ip_address INET
 # );
 
-# -- Комментарии к таблице и колонкам (документация)
-# COMMENT ON COLUMN account.id_account IS 'ID аккаунта (автоинкремент)';
-# COMMENT ON COLUMN account.login IS 'Логин пользователя';
-# COMMENT ON COLUMN account.pswd IS 'Пароль (рекомендуется хранить хеш)';
-# COMMENT ON COLUMN account.creation_date IS 'Дата и время создания аккаунта';
-# COMMENT ON COLUMN account.is_blocked IS 'Статус блокировки аккаунта';
-# COMMENT ON COLUMN account.last_access_time IS 'Время последнего доступа к аккаунту';
-# COMMENT ON COLUMN account.ip_address IS 'IP-адрес (поддерживает ipv4 и ipv6)';
+# COMMENT ON COLUMN user_account.id_account IS 'ID аккаунта (автоинкремент)';
+# COMMENT ON COLUMN user_account.login IS 'Логин пользователя';
+# COMMENT ON COLUMN user_account.pswd IS 'Пароль (рекомендуется хранить хеш)';
+# COMMENT ON COLUMN user_account.creation_date IS 'Дата и время создания аккаунта';
+# COMMENT ON COLUMN user_account.is_blocked IS 'Статус блокировки аккаунта';
+# COMMENT ON COLUMN user_account.last_access_time IS 'Время последнего доступа к аккаунту';
+# COMMENT ON COLUMN user_account.ip_address IS 'IP-адрес (поддерживает ipv4 и ipv6)';
 
-# -- Индекс для быстрого поиска по логину (ускоряет авторизацию)
-# CREATE INDEX idx_account_login ON account(login);
+# CREATE INDEX idx_user_account_login ON user_account(login);
+# CREATE INDEX idx_user_account_last_access ON user_account(last_access_time);
+# CREATE INDEX idx_user_account_ip ON user_account(ip_address);
 
-# -- Индекс для фильтрации по блокировке и последнему доступу (полезно для очистки старых аккаунтов)
-# CREATE INDEX idx_account_last_access ON account(last_access_time);
-
-# -- Индекс для поиска по IP адресу (логирование безопасности)
-# CREATE INDEX idx_account_ip ON account(ip_address);
-
-# -- Вставка данных
-# INSERT INTO account (login, pswd, creation_date, is_blocked, last_access_time, ip_address)
+# INSERT INTO user_account (login, pswd, creation_date, is_blocked, last_access_time, ip_address)
 # VALUES ('joh_doe', 'secure_pas', '2024-01-15 10:30:00', false, '2024-01-15 10:30:00', '10.0.0.1');
 
-# -- Просмотр данных
-# SELECT * FROM account;
+# SELECT * FROM user_account;
