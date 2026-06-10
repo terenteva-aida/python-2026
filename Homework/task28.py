3f25cef45657faa800b1cfd18018eeb81e3688fa
Aida <terenteva.aida.2401@gmail.com>
2026-06-10 21:26:36 +0300

    .

--- Homework/task28.py
+++ Homework/task28.py
@@ -3,12 +3,41 @@
 # todo 2: В игровой матрице nxn найти кол-во всех 1
 
 #  Задачи решить через генераторы списков (списковые включения)
-
+    
 import random
 
 def create_random_field(n, num_ships):
-    # Поле из нулей
-    field = [[0 for i in range (n)] for i in range(n)]
-    # Координаты для кораблей
+    # Создаём поле n x n, заполненное нулями (списковое включение)
+    field = [[0 for _ in range(n)] for _ in range(n)]
+    
+    # Генерируем все возможные координаты (списковое включение)
     all_ships = [(i, j) for i in range(n) for j in range(n)]
     
+    # Случайно выбираем num_ships уникальных координат для кораблей
+    ship_positions = random.sample(all_ships, num_ships)
+    
+    # Расставляем корабли (единицы) на поле
+    for i, j in ship_positions:
+        field[i][j] = 1
+    
+    return field
+
+def count_ships(field):
+    # Подсчёт количества единиц через списковое включение и функцию len
+    return len([cell for row in field for cell in row if cell == 1])
+
+if __name__ == "__main__":
+    n = 5          # размер поля
+    num_ships = 5  # количество кораблей (единиц)
+    
+    # Создаём поле
+    battlefield = create_random_field(n, num_ships)
+    
+    # Выводим поле построчно
+    print("Игровое поле:")
+    for row in battlefield:
+        print(row)
+    
+    # Считаем количество единиц
+    ships_count = count_ships(battlefield)
+    print(f"\nКоличество кораблей (единиц): {ships_count}")
--- Homework/task29.py
+++ Homework/task29.py
@@ -11,3 +11,32 @@
 # ||1 2 3||      ||1 4||
 # ||4 5 6||  =>  ||2 5||
 #                ||3 6||
+
+def transpose(matrix):
+    """
+    Транспонирует матрицу, используя списковое включение.
+    
+    Параметры:
+    matrix (list of list): Исходная матрица размером m x n.
+    
+    Возвращает:
+    list of list: Транспонированная матрица размером n x m.
+    """
+    # Проверка, что матрица не пуста
+    if not matrix or not matrix[0]:
+        return []
+    
+    # Списковое включение: для каждого столбца j создаём строку из элементов всех строк в этом столбце
+    return [[matrix[i][j] for i in range(len(matrix))] for j in range(len(matrix[0]))]
+
+if __name__ == "__main__":
+    original = [[1, 2, 3], [4, 5, 6]]
+    transposed = transpose(original)
+    
+    print("Исходная матрица:")
+    for row in original:
+        print(row)
+    
+    print("\nТранспонированная матрица:")
+    for row in transposed:
+        print(row)
